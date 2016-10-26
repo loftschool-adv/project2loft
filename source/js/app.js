bce15ff712e0ba8f144ec2fe7f98400e9bca417b
AnyaGorshkova <anua_spb@mail.ru>
2016-10-27 03:13:16 +0700

    add ajax for registration

--- source/js/app.js
+++ source/js/app.js
@@ -1,12 +1,164 @@
 (function(){
+		 $('.popup__link_registr').click(function(e){
+		 	e.preventDefault();
+	 	$('.flipper-container').addClass('flipp');
+	 });
+		 $('.popup__link_enter').click(function(e){
+		 	e.preventDefault();
+	 	$('.flipper-container').removeClass('flipp');
+	 });
+})();
 
 
-	 $('.popup__link_registr').click(function(e){
-	 	e.preventDefault();
- 	$('.flipper-container').addClass('flipp');
- });
-	 $('.popup__link_enter').click(function(e){
-	 	e.preventDefault();
- 	$('.flipper-container').removeClass('flipp');
- });
-	})();
+// модуль валидации
+var validation = (function() {
+	'use strict';
+
+	var init = function() {
+		_setUpListners();
+	};
+	var _setUpListners = function() {
+		//$('input').on('keydown',function(){
+		//})
+	};
+
+// функция валидации формы
+var validateForm = function(form) {
+	var 
+			elements = form.find('input'),
+			valid = true;
+// Проверяем все input
+	$.each(elements, function(index, val){
+		var
+				element = $(val),
+				val = element.val();
+		if((val.length === 0)) {
+			console.log('не валидна');
+			valid = false;
+		}
+	})
+	return valid;
+};
+
+var clearForm = function(form) {
+	
+	var 
+			elements = form.find('input');
+
+	$.each(elements, function(index, val){
+		var
+				element = $(val),
+				val = element.val();
+		element.val('');
+		});
+	};
+	return {
+		init: init,
+		validateForm: validateForm,
+		clearForm: clearForm
+	};
+})();
+validation.init();
+
+
+//Регистрация нового пользователя
+var registration = (function() {
+	'use strict';
+
+	var init = function() {
+		_setUpListners();
+	};
+
+	var _setUpListners = function() {
+		$('#registration').on('click', _submitForm);
+	};
+
+	var _submitForm = function(ev){
+		ev.preventDefault();
+		var form = $(this).parent(),
+				url = '/reg/',
+				data = 
+				{
+	    		login: form.find('input[name = "login"]').val(),
+	    		email: form.find('input[name = "email"]').val(),
+	    		pass: form.find('input[name = "pass"]').val(),
+	   		},
+				servAns = _ajaxForm(form, url, data);
+				if(servAns){
+					console.log('выводим ответ от сервера');
+					servAns.done(function(ans) {
+						console.log(ans);
+				})
+			}	
+	}
+	var _ajaxForm = function (form, url, data){
+		//если валидация прошла успешно, отправляем запрос на сервер
+		if (!validation.validateForm(form)) return false;
+		console.log('всё хорошо');
+		// готовим данные 
+	  data=JSON.stringify(data);
+	  console.log(data);
+	  // отправляем
+		return $.ajax({
+			url: '/reg/',
+			type: 'POST',
+			contentType: 'application/json',
+			data: data
+		});
+		//validation.clearForm(form);
+	}
+
+	return {
+
+		init: init
+	};
+})();
+registration.init();
+
+//Логин
+var login = (function() {
+	'use strict';
+
+	var init = function() {
+		_setUpListners();
+	};
+
+	var _setUpListners = function() {
+		$('#login').on('click', _submitForm);
+	};
+
+	var _submitForm = function(ev){
+		ev.preventDefault();
+		var form = $(this).parent(),
+				url = ' ',
+				data = 
+				{
+	    		email: $('input[name = "email"]').val(),
+	    		pass: $('input[name = "pass"]').val(),
+	   		},
+				servAns = _ajaxForm(form, url, data);
+				if(servAns){
+					console.log('выводим ответ от сервера');
+					servAns.done(function(ans) {
+					console.log(ans);
+				})
+			}	
+	}
+	var _ajaxForm = function (form, url, data){
+		//если валидация прошла успешно, отправляем запрос на сервер
+		if (!validation.validateForm(form)) return false;
+		console.log('всё хорошо');
+		// готовим данные 
+	  data=JSON.stringify(data);
+	  console.log(data);
+	  // отправляем
+		
+		//validation.clearForm(form);
+	}
+
+	return {
+
+		init: init
+	};
+})();
+login.init();
