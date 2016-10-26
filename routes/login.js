81f9ea1babb105a776785388e9ad36f591570a72
Eclerr <eclerrrr@yandex.ru>
2016-10-26 05:18:55 +0300

    add validation password

--- routes/login.js
+++ routes/login.js
@@ -7,6 +7,15 @@ let route = require('express').Router();
 let mongoose = require('mongoose');
 
 
+let sendMasage = function(message, res, status = 0){
+	res.json(
+		{ 
+			message: message,
+			status: status
+		})
+}
+
+
 
 // Обращаемся к корню сайта , и рендерим шаблон из ./templates/pages/index.pug
 /*route.get('', (req,res) =>{
@@ -23,13 +32,17 @@ route.post('/reg/', (req,res) =>{
 	let User = require('../models/user.js').User;
 	for(let key in req.body){
 		if(!req.body[key]){
-			return res.json({ message: 'Заполнены не все поля' });
+			return sendMasage('Заполнены не все поля' , res, 1);
+			//return res.json({ message: 'Заполнены не все поля' });
 		};
 	}
 	User.findOne({'login' : req.body.login}).then((item) => {
 		if(item){
-			return res.json({ message: 'Такой пользователь уже существует' });
+			return sendMasage('Такой пользователь уже существует' , res , 2);
 		}else{
+			if(Object.keys(req.body.pass).length < 8){
+				return sendMasage('Пароль должен содержать не менее 8 символов' , res , 3);
+			}
 			let user = new User({
 			    login : req.body.login,
 			    password: req.body.pass,
@@ -37,7 +50,7 @@ route.post('/reg/', (req,res) =>{
 			})
 			user.save(function( err, user, affected){
     		if (err) throw err;
-    		return res.json({ message: 'Вы успешно зарегистрированы' });
+    			return sendMasage('Вы успешно зарегистрированы' , res);
 			});
 		}
 	})
