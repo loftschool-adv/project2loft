2dfcdd1201637182a70e572796950fd29bd7a80f
Artem Boyur <gboyur@gmail.com>
2016-10-26 19:15:05 +0300

    merge all branch complete

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -2,7 +2,7 @@
 
 module.exports = function() {
   $.gulp.task('sass', function() {
-    return $.gulp.src('./source/style/index.scss')
+    return $.gulp.src('./source/style/main.scss')
       .pipe($.gp.sourcemaps.init())
       .pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' }))
       .pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig }))
