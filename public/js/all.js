305d1416a549890529766561e7e6c62b43cd0964
Anton Ivchenko <a.a.ivchenko@gmail.com>
2016-10-27 17:51:12 +0300

    feat(header): add animation for the button Home

---
+++ public/js/all.js
@@ -0,0 +1,23 @@
+// =================== USER HEADER BUTTON HOME START===========================================
+// The function change the form and the color of background if the mouse over upon the button Home at User page
+// Анимация конпки Домой на странице user
+"use strict";
+var buttonHomeAnimate = (function () {
+    let button = document.querySelector('.user__header__top__button-home');
+    let hoverElem = document.querySelector('.user__header__top__button-home__hover');
+    let homeSVG = document.querySelector('.user__header__top__button-home__inner__path');
+    return {
+        'init': function () {
+            button.addEventListener('mouseenter', function () {
+                homeSVG.style.fill = '#ffffff';
+                hoverElem.style.opacity = '1';
+            });
+            button.addEventListener('mouseleave', function () {
+                hoverElem.style.opacity = '0';
+                homeSVG.style.fill = '#a0a09f';
+            });
+        }
+    }
+})();
+buttonHomeAnimate.init();
+// =================== USER HEADER BUTTON HOME END===========================================
