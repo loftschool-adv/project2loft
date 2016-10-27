dbe7d106c306eb6c5cf162f50cfe3fb96064f594
Eclerr <eclerrrr@yandex.ru>
2016-10-27 05:42:20 +0300

    fix 404

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ let log = require('./libs/log')(module);
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'templates/pages'));
+app.set('views', path.join(__dirname, 'templates/pages/'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
@@ -27,12 +27,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //===маршруты===
+app.use('/user', require('./routes/user.js'));
 app.use('/', require('./routes/login.js'));
-//app.use('/user', require('./routes/user.js'));
 //=============
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next)  => {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -43,7 +43,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -54,7 +54,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
