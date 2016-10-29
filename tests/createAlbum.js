01be45e80cde523bfadc36d58e87272ebbe5a3a6
Artem Boyur <gboyur@gmail.com>
2016-10-29 13:41:22 +0300

    add new model album, add test script create album

--- tests/createAlbum.js
+++ tests/createAlbum.js
@@ -1,3 +1,18 @@
-/**
- * Created by boyur on 29.10.16.
- */
+// Данный скрип работает с базой описанной в moongoose.js
+// Скрипт создан в целях тестирования
+
+// Подключаем файл с моделью юзеров
+let mongoose = require('../modules/libs/mongoose.js');
+let Album = require('../modules/models/album.js').Album;
+
+// Создаем экземпляр пользователя
+let album = new Album({
+  name : 'Tester',
+  user_id: '5814492c3447a7e857388a3d'
+});
+
+// Сохраняем пользователя в базу
+album.save(function( err, album, affected){
+  if (err) throw err;
+  console.log('Создан album - Tester')
+});
