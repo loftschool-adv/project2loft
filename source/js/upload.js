bfd70b7cf8021afca48ee8146a1b7d7a4491b4b4
Artem Boyur <gboyur@gmail.com>
2016-11-02 19:57:17 +0300

    resize preload img

--- source/js/upload.js
+++ source/js/upload.js
@@ -52,7 +52,7 @@ function updateProgress(bar, value) {
     // а также создаем ему свойство file, куда помещаем объект File (при загрузке понадобится)
     var li = $('<li/>').appendTo(imgList);
    // $('<div/>').text(file.name).appendTo(li);
-    var img = $('<img/>').appendTo(li);
+    var img = $('<canvas class="myCanvas"/>').appendTo(li);
     $('<div/>').addClass('progress').text('0%').appendTo(li);
     li.get(0).file = file;
 
@@ -61,7 +61,8 @@ function updateProgress(bar, value) {
     var reader = new FileReader();
     reader.onload = (function(aImg) {
       return function(e) {
-        aImg.attr('src', e.target.result);
+        let src = render(e.target.result, i);
+        aImg.attr('src', src);
         aImg.attr('width', 100);
         /* ... обновляем инфу о выбранных файлах ... */
       };
@@ -104,6 +105,26 @@ $("#upload").click(function() {
     });
   });
 });
+
+// Resize preload
+var MAX_HEIGHT = 100;
+function render(src, i){
+  var image = new Image();
+  image.onload = function(){
+    var canvas = document.getElementsByClassName("myCanvas");
+    if(image.height > MAX_HEIGHT) {
+      image.width *= MAX_HEIGHT / image.height;
+      image.height = MAX_HEIGHT;
+    }
+    var ctx = canvas[i].getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    canvas[i].width = image.width;
+    canvas[i].height = image.height;
+    ctx.drawImage(image, 0, 0, image.width, image.height);
+  };
+  image.src = src;
+}
+
 
 
 // Проверка поддержки File API в браузере
