17089dea36700ddc10b1b5b807eb8216150b672c
Artem Boyur <gboyur@gmail.com>
2016-11-07 11:30:26 +0300

    fix socket.io

--- source/js/app.js
+++ source/js/app.js
@@ -10,7 +10,30 @@
 // 8) Для того чтобы модуль собрался в один файл app.js его нужно прописать в в gulpfile.js.
 // Документация по фунциям base, будет чуть позже...
 
+var socket = io.connect('http://85.143.214.16:4000');
 
+socket.on('eventClient', function (data) {
+
+	var src = data.thumb;
+	src =String(src).replace(/\\/g, "/");
+	src = src.substr(6);
+	console.log(src);
+
+	var li = $('<li/>').addClass('img-item').appendTo($('ul#img-list'));
+	var ImgCont = $('<div/>').addClass('img-cont').appendTo(li);
+	var image =$('<img>', {
+		src: '/'+src});
+
+	// Когда картинка загрузится, ставим её на фон
+	image.on("load", function(){
+		ImgCont.css('background-image', 'url("/'+src+'")');
+	});
+	$('.modal__load-img').hide();
+
+});
+socket.emit('eventServer', {data: 'Hello Server'});
+
+//////////////////////////////////////////////////////////
 
 $( document ).ready(function() {
     var base = new BaseModule; // Инициализируем библиотеку. (Пока не нужно)
