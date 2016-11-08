45e904a2f36af7297ddaa30f8d105b7cd04a8d9b
Artem Boyur <gboyur@gmail.com>
2016-11-08 14:52:38 +0300

    fix upload img, async upload

--- source/js/upload.js
+++ source/js/upload.js
@@ -42,150 +42,18 @@ if (isAdvancedUpload) {
 // Ручная отправка
 $form.on('submit', function(e) {
   if ($form.hasClass('is-uploading')) return false;
-
-  //alert('Отправляем');
-
   $form.addClass('is-uploading').removeClass('is-error');
 
   if (isAdvancedUpload) {
     e.preventDefault();
-
-
-
     var photos = $input[0].files;
 
-    async.each(photos, function(photo, callback) {
-
-      var ajaxData = new FormData();
-      ajaxData.append("photo", photo);
-
-      $.ajax({
-        url: location.href + '/addImg/',
-        type: $form.attr('method'),
-        data: ajaxData,
-        dataType: 'json',
-        cache: false,
-        contentType: false,
-        processData: false,
-        xhr: function() {
-          // create an XMLHttpRequest
-          var xhr = new XMLHttpRequest();
-
-          // listen to the 'progress' event
-          xhr.upload.addEventListener('progress', function(evt) {
-
-            if (evt.lengthComputable) {
-              // calculate the percentage of upload completed
-              var percentComplete = evt.loaded / evt.total;
-              percentComplete = parseInt(percentComplete * 100);
-
-              console.log(percentComplete);
-
-              // update the Bootstrap progress bar with the new percentage
-              //$('.progress-bar').text(percentComplete + '%');
-              //$('.progress-bar').width(percentComplete + '%');
-
-              // once the upload reaches 100%, set the progress bar text to done
-              if (percentComplete === 100) {
-                //$('.progress-bar').html('Done');
-              }
-
-            }
-
-          }, false);
-
-          return xhr;
-        },
-        complete: function(ans) {
-          $form.removeClass('is-uploading');
-          console.log(ans.responseText);
-
-
-
-          //socket.emit('eventServer', {data: 'Hello Server'});
-        },
-        success: function(data) {
-
-          $form.addClass( data.success == true ? 'is-success' : 'is-error' );
-
-          callback('upload');
-
-          if (!data.success) $errorMsg.text(data.error);
-        },
-        error: function() {
-          // Log the error, show an alert, whatever works for you
-        }
-      });
-
-    });
-
-    // $.each( photos, function( key, photo ) {
-    //   var ajaxData = new FormData();
-    //   ajaxData.append("photo", photo);
-    //
-    //   $.ajax({
-    //     url: location.href + '/addImg/',
-    //     type: $form.attr('method'),
-    //     data: ajaxData,
-    //     dataType: 'json',
-    //     cache: false,
-    //     contentType: false,
-    //     processData: false,
-    //     complete: function(ans) {
-    //       $form.removeClass('is-uploading');
-    //       console.log(ans.responseText);
-    //
-    //
-    //
-    //       //socket.emit('eventServer', {data: 'Hello Server'});
-    //     },
-    //     success: function(data) {
-    //
-    //       $form.addClass( data.success == true ? 'is-success' : 'is-error' );
-    //
-    //       if (!data.success) $errorMsg.text(data.error);
-    //     },
-    //     error: function() {
-    //       // Log the error, show an alert, whatever works for you
-    //     }
-    //   });
-    //
-    // });
+    ajaxUploadImg(photos);
 
     if (droppedFiles) {
-      $.each( droppedFiles, function(i, file) {
-
-        console.log($input.attr('name'));
-
-        ajaxData.append( $input.attr('name'), file );
-
-      });
+      ajaxUploadImg(droppedFiles);
     }
 
-    //console.log(ajaxData);
-
-
-
-    console.log($form.attr('action'));
-
-  } else {
-
-    var iframeName  = 'uploadiframe' + new Date().getTime();
-    $iframe   = $('<iframe name="' + iframeName + '" style="display: none;"></iframe>');
-
-    $('body').append($iframe);
-    $form.attr('target', iframeName);
-
-    $iframe.one('load', function() {
-      var data = JSON.parse($iframe.contents().find('body' ).text());
-      $form
-        .removeClass('is-uploading')
-        .addClass(data.success == true ? 'is-success' : 'is-error')
-        .removeAttr('target');
-      if (!data.success) $errorMsg.text(data.error);
-      $form.removeAttr('target');
-      $iframe.remove();
-    });
   }
 });
 
@@ -208,3 +76,60 @@ $save.on('click', function () {
   });
 
 });
+
+function ajaxUploadImg(photos) {
+  async.eachSeries(photos, function(photo, callbackEach) {
+
+    $('.modal__load-img').hide();
+    var li = $('<li/>').addClass('img-item').appendTo($('ul#img-list'));
+    var ImgCont = $('<div/>').addClass('img-cont').appendTo(li);
+
+    var ajaxData = new FormData();
+    ajaxData.append("photo", photo);
+
+    $.ajax({
+      url: location.href + '/addImg/',
+      type: $form.attr('method'),
+      data: ajaxData,
+      dataType: 'json',
+      cache: false,
+      contentType: false,
+      processData: false,
+      complete: function(data) {
+        $form.removeClass('is-uploading');
+
+        ////////////////////////////////////////////
+        ////////////////////////////////////////////
+        var src = data.responseText;
+        src =String(src).replace(/\\/g, "/");
+        src = src.substr(6);
+        console.log(src);
+
+        var image =$('<img>', {
+          src: '/'+src});
+
+        // Когда картинка загрузится, ставим её на фон
+        image.on("load", function(){
+          ImgCont.css('background-image', 'url("/'+src+'")');
+        });
+
+        ///////////////////////////////////////////
+        ////////////////////////////////////////////
+
+        callbackEach();
+
+        //socket.emit('eventServer', {data: 'Hello Server'});
+      },
+      success: function(data) {
+
+        $form.addClass( data.success == true ? 'is-success' : 'is-error' );
+
+        if (!data.success) $errorMsg.text(data.error);
+      },
+      error: function() {
+        // Log the error, show an alert, whatever works for you
+      }
+    });
+
+  });
+}
