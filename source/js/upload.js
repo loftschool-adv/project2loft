294579df7df6e404381c995346e92aa712449d64
Artem Boyur <gboyur@gmail.com>
2016-11-08 07:17:35 +0300

    add progress bar

--- source/js/upload.js
+++ source/js/upload.js
@@ -67,6 +67,35 @@ $form.on('submit', function(e) {
         cache: false,
         contentType: false,
         processData: false,
+        xhr: function() {
+          // create an XMLHttpRequest
+          var xhr = new XMLHttpRequest();
+
+          // listen to the 'progress' event
+          xhr.upload.addEventListener('progress', function(evt) {
+
+            if (evt.lengthComputable) {
+              // calculate the percentage of upload completed
+              var percentComplete = evt.loaded / evt.total;
+              percentComplete = parseInt(percentComplete * 100);
+
+              console.log(percentComplete);
+
+              // update the Bootstrap progress bar with the new percentage
+              //$('.progress-bar').text(percentComplete + '%');
+              //$('.progress-bar').width(percentComplete + '%');
+
+              // once the upload reaches 100%, set the progress bar text to done
+              if (percentComplete === 100) {
+                //$('.progress-bar').html('Done');
+              }
+
+            }
+
+          }, false);
+
+          return xhr;
+        },
         complete: function(ans) {
           $form.removeClass('is-uploading');
           console.log(ans.responseText);
